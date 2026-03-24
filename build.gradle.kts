679ce0d4a436f451b7c3f291f3793591dd324708
TheSharkHour <253013896+TheSharkHour@users.noreply.github.com>
2026-03-24 15:47:36 -0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,8 +6,8 @@ import java.net.URL
 
 plugins {
 	id("maven-publish")
-	id("fabric-loom") version "1.13.3"
-	id("babric-loom-extension") version "1.13.3"
+	id("fabric-loom") version "1.15.3"
+	id("babric-loom-extension") version "1.15.3"
 }
 
 //noinspection GroovyUnusedAssignment
